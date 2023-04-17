ec04a25aec4d82b04dc8291769ca2867c2b7bbbd
Myun9hyun <126433920+Myun9hyun@users.noreply.github.com>
2023-04-17 03:52:02 +0000

    어캐하누..

--- Main.java
+++ Main.java
@@ -2,7 +2,8 @@ class Main {
   public static void main(String[] args) {
 
     System.out.println("Hello playdata");
-    System.out.println("Hello playdata");
+    System.out.println("Hello World");
+    System.out.println("Hello Java");
   }
   
 }
